ef9b9d3f2b94d3a0a1d5e6979e1d29b34e2f0db1
larrygf02 <raulgf_02@hotmail.com>
2019-05-12 18:12:41 -0500

    Configuracion del socket io +1

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ app.use(express.static(publicPath));
 // IO = esta es la comunicacion del backend
 let io = socketIO(server)
 
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
+})
 
 server.listen(port, (err) => {
 
