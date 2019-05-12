94c4f33e65173a40f2aaa4a53a5adcb2819f27f7
larrygf02 <raulgf_02@hotmail.com>
2019-05-12 18:44:15 -0500

    Emitir desde el cliente al servidor

--- server/server.js
+++ server/server.js
@@ -21,6 +21,11 @@ io.on('connection', (client) => {
     client.on('disconnect', () => {
         console.log('Usuario desconectado');
     })
+
+    // Escuchar el cliente
+    client.on('enviarMensaje', (mensaje) => {
+        console.log(mensaje);
+    })
 })
 server.listen(port, (err) => {
 
